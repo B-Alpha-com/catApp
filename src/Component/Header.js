cffec5914d884ff7da2da7f39cfd5399cefda196
B-Alpha-com <olasodem2@yahoo.com>
2021-02-21 14:35:38 +0100

    build and style

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -2,29 +2,27 @@ import React from "react";
 
 const Header = ({ onclick, onsearch, value, keyPress }) => {
   return (
-    <div>
-      <nav className="nav_1">
-        <ul className="Unorder">
-          <li className="text">SheisaCatLover</li>
-          <li>
-            <input
-              className="input"
-              type="text"
-              placeholder="search Your Imagination"
-              title="I will always love you"
-              onChange={onsearch}
-              value={value}
-              onKeyPress={keyPress}
-            />
-          </li>
-          <li>
-            <button id="enterKey" Title="search for Cats" onClick={onclick}>
-              Enter
-            </button>
-          </li>
-          <li></li>
-        </ul>
-      </nav>
+    <div className="nav_1">
+      <ul className="Unorder">
+        <li className="text">SheisaCatLover</li>
+        <li>
+          <input
+            className="input"
+            type="text"
+            placeholder="search Your Imagination"
+            title="I will always love you"
+            onChange={onsearch}
+            value={value}
+            onKeyPress={keyPress}
+          />
+        </li>
+        <li>
+          <button id="enterKey" Title="search for Cats" onClick={onclick}>
+            Enter
+          </button>
+        </li>
+        <li></li>
+      </ul>
     </div>
   );
 };
